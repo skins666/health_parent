af322ca5b5722ba6bc6550a4830357a576305ddf
rsz <2445400433@qq.com>
2023-05-03 18:52:32 +0800

    骨架

--- health_parent/health_common/src/main/java/com/itheima/Main.java
+++ health_parent/health_common/src/main/java/com/itheima/Main.java
@@ -1,4 +1,6 @@
-package com.itheima;public class Main {
+package com.itheima;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
